17374fcf3f750b5fb314e3c64f3153da0e5daf30
Harshit Yadav <121128576+HarshitYaadav@users.noreply.github.com>
2023-10-31 01:17:09 +0530

    Create Firebase_Login.kt

---
+++ Firebase_Login.kt
@@ -0,0 +1,51 @@
+package com.example.firebaselog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.firebaselog.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.LoginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if (email.isNotEmpty()&&password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this){task->
+                        if (task.isSuccessful){
+                            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this , MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+        binding.signupText.setOnClickListener{
+            startActivity(Intent(this,SignUpActivity::class.java))
+            finish()
+        }
+    }
+}
